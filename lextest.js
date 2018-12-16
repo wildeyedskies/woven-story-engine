43a1d81d162c6c42fdb4599dd6435bfa1deeb742
Zoe <zoenb@mailbox.org>
2018-12-16 15:11:59 -0500

    added a todo, worked on the lexer a bit

--- lextest.js
+++ lextest.js
@@ -7,11 +7,13 @@ let lexer = new Lexer;
 let root = {children: [], type: 'root'}
 let lastNode = root
 
+// Lexer Macro Rules
+
 lexer.addRule(/\\section\s?\([^\n)]+\)\s*/, (text) => {
    let args = text.slice(text.indexOf('(') + 1, text.indexOf(')')).split(',').map(i => i.trim())
 
    lastNode.children.push({type: 'section', name: args[0], args: args.slice(1), variables: [], children: [], parentNode: lastNode})
-   })
+})
 
 lexer.addRule(/\\set\s?\([^\n)]+\)/, (text) => {
    // set can't have any children
@@ -31,9 +33,29 @@ lexer.addRule(/\\show\s?\([^\n)]+\)\s*/, (text) => {
       preserveLinkText: args[1], args: args.slice(2), children: [], parentNode: lastNode})
 })
 
+lexer.addRule(/\\include\s?\([^\n)]+\)\s*/, (text) => {
+   let args = text.slice(text.indexOf('(') + 1, text.indexOf(')')).split(',').map(i => i.trim())
+   
+   lastNode.children.push({type: 'include', name: args[0], args: args.slice(1), children: [], parentNode: lastNode})
+})
+
+lexer.addRule(/\\if\s?\([^\n)]+\)\s*/, (text) => {
+   let condition = text.slice(text.indexOf('(') + 1, text.indexOf(')'))
+
+   lastNode.children.push({type: 'if', condition: condition, children: [], parentNode: lastNode})
+})
+
+lexer.addRule(/\\else\s*/, (text) => {
+   //TODO make sure there's an if block before the else
+   lastNode.children.push({type: 'else', condition: condition, children: [], parentNode: lastNode})
+})
+
+
 lexer.addRule(/\\choices\s*/, (text) => {
    lastNode.children.push({type: 'choices', children: [], parentNode: lastNode})
 })
+
+// Text modifiers
 
 lexer.addRule(/\\h1\s*/, () => {
    lastNode.children.push({type: 'h1', children: [], parentNode: lastNode})
@@ -44,9 +66,18 @@ lexer.addRule(/\\h2\s*/, () => {
 })
 
 
+lexer.addRule(/\\em\s*/, () => {
+   lastNode.children.push({type: 'em', children: [], parentNode: lastNode})
+})
+
+lexer.addRule(/\\bf\s*/, () => {
+   lastNode.children.push({type: 'bf', children: [], parentNode: lastNode})
+})
+
+// Brackets
 lexer.addRule(/(\s|\n)*\{/, () => {
    if (lastNode.children.length < 1) throw "Cannot begin block. Your brackets are likely unbalanced"
-   if (lastNode.type === 'text') throw "Invalid {"
+   if (lastNode.type === 'text') throw "Invalid { following text block"
    lastNode = lastNode.children[lastNode.children.length -1]
 })
 
@@ -55,11 +86,13 @@ lexer.addRule(/\}/, () => {
    lastNode = lastNode.parentNode
 })
 
+// Text
 lexer.addRule(/[^(\n\n){}]+/, (text) => {
    // text can't have children
    lastNode.children.push({type: 'text', 'text': text, parentNode: lastNode})
 })
 
+// Newline
 lexer.addRule(/\n(\n+)/, () => {
    lastNode.children.push({type: 'lineBreak', parentNode: lastNode})
 })
@@ -84,18 +117,42 @@ function process(node) {
 }
 
 function processH1(node) {
-   content = node.children.map(n => process(n)).reduce((acc, n) => acc + n, '')
+   let content = node.children.map(n => process(n)).reduce((acc, n) => acc + n, '')
    return `<h1>${content}</h1>`
 }
 
 function processH2(node) {
-   content = node.children.map(n => process(n)).reduce((acc, n) => acc + n, '')
+   let content = node.children.map(n => process(n)).reduce((acc, n) => acc + n, '')
    return `<h2>${content}</h2>`
 }
 
+
+function processEM(node) {
+   let content = node.children.map(n => process(n)).reduce((acc, n) => acc + n, '')
+   return `<em>${content}</em>`
+}
+
+function processBF(node) {
+   let content = node.children.map(n => process(n)).reduce((acc, n) => acc + n, '')
+   return `<strong>${content}</strong>`
+}
+
 function processSection(section) {
-  content = section.children.map(n => process(n)).reduce((acc, n) => acc + n, '')
+  // first let's process each of the child nodes
+  let children = section.children.map(n => process(n))
+  // now we need to iterate through them and group them into 
+  
   return `function ${section.name}(${section.args.join(',')}) { ${section.variables.join(';')}; return \`${content}\` }`
+}
+
+function processNav(node) {
+  let content = node.children.map(n => process(n)).reduce((acc, n) => acc + n, '')
+  return `<a onclick="navigate(${node.name}(${node.args.join(',')}))">${content}</a>`
+}
+
+function processShow(node) {
+  let content = node.children.map(n => process(n)).reduce((acc, n) => acc + n, '')
+  return `<a onclick="show(${node.name}(${node.args.join(',')}), node.preserveLinkText)">${content}</a>`
 }
 
 function processRoot(root) {
